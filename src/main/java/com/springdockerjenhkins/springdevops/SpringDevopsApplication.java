80db86cd5e8d4b8686134a2829fe38c9a2aab85c
lilndabose <lilndabose@gmail.com>
2023-03-02 13:30:24 +0400

    Second app commit

--- src/main/java/com/springdockerjenhkins/springdevops/SpringDevopsApplication.java
+++ src/main/java/com/springdockerjenhkins/springdevops/SpringDevopsApplication.java
@@ -2,10 +2,12 @@ package com.springdockerjenhkins.springdevops;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringDevopsApplication {
@@ -20,6 +22,11 @@ public class SpringDevopsApplication {
 	@PreDestroy
 	public void destroyApp(){
 		logger.warn("Application is getting destroyed !!!");
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 	public static void main(String[] args) {
